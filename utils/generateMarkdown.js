c1d9fad0252669496c7377f7ceeca63e2404cd47
annia valdes <millerl.mb79@gmail.com>
2020-07-17 22:52:13 -0500

    add the information for write in to readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,8 +1,37 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.projecttitle}
 
+  ### Description
+  ${data.description}
+
+  ### Table of Contents
+  
+
+  ### Installation
+  ${data.installation}
+
+  ### Usage
+  ${data.usage}
+
+  ### License
+  ${data.license}
+
+  ### Contributing 
+  ${data.contribution}
+
+  ### Tests
+  ${data.tests}
+
+  ### Questions
+  If you have questions about the project, below you can find ways to answer them, either by visiting my highub or contacting me by email
+  
+  Link to my Github:[${data.github}](https://github.com/${data.github})
+  
+  Email acount: ${data.email}
+    
 `;
+
 }
 
 module.exports = generateMarkdown;
